ce17bd5ed3fa554e6b75049743a773d088882607
nsshayan@gmail.com <nsshayan@gmail.com@238d075a-4c7a-7ccf-3d0c-72447ac59adc>
2012-11-24 10:35:01 +0000

    committing the funcPrototype3.h

---
+++ trunk/include/funcPrototype3.h
@@ -0,0 +1,29 @@
+extern int error_createvfs(char *,int );
+
+extern int error_mountvfs(char *);
+
+extern int error_unmountvfs(char *);
+
+extern int error_makedir(char *,char *);
+
+extern int error_deletedir(char *);
+
+extern int error_movedir(char *, char *);
+
+extern int error_listdir(char *, int , char *);
+
+extern int error_addfile(char *, char *, char *);
+
+extern int error_listfile(char *, char *);
+
+extern int error_updatefile(char *, char *);
+
+extern int error_removefile(char *);
+
+extern int error_movefile(char *,char *);
+
+extern int error_copyfile(char *,char *);
+
+extern int error_exportfile(char *,char *);
+
+extern int error_searchfile(char *,char *);
