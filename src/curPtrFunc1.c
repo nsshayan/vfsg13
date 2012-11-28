117f24ed43aa6aa533e4b5313d0f71ac22e859b5
prashantkumar117@gmail.com <prashantkumar117@gmail.com@238d075a-4c7a-7ccf-3d0c-72447ac59adc>
2012-11-28 10:27:00 +0000

    curPtrFunc1

---
+++ src/curPtrFunc1.c
@@ -0,0 +1,31 @@
+#include "mainHeader.h"
+#include "funcPrototype.h"
+#include "funcPrototype2.h"
+
+struct nAryTree *curPtrFunc1(char P1[],struct nAryTree *temp) {
+
+	char tempP1[100],tempP2[100];
+	struct nAryTree *curPtr;
+	curPtr=(struct nAryTree *)malloc(sizeof(struct nAryTree));
+
+	char **token;
+	int count,n;	
+
+	count=countSlash(P1);
+	token=stringtok(P1);
+
+	if (count>0) {
+		n=strlen(P1)-(strlen(token[count])+1);
+		strcpy(tempP2,token[count]);
+		strncpy(tempP1,P1,n);		
+	}
+	else {
+		strcpy(tempP2,"root");
+		strcpy(tempP1,"root");		
+	}
+
+	curPtr=currPtrReturn(tempP1,tempP2);
+
+return curPtr;
+
+}
